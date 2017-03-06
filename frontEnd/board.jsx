b68376df8eac47316480fdc930c49fd6faf8cf03
Rachel <racheldepriest1@gmail.com>
2017-03-06 17:02:55 -0600

    made more changes to board

--- frontEnd/board.jsx
+++ frontEnd/board.jsx
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 //visualization of Board
 
 //[0, 0, 0]
@@ -5,43 +7,41 @@
 //[0, 0, 0]
 
 
-import { Component } from 'react';
-
 class Squares extends Component{
 	render(){
 		return(){
-			<div className = "squares"></div>
+			<div className="squares"></div>
 		}
 	}
 }
 
-class Board extends Component{
-	render(){
-		//create a function to check whether or not one of the squares has been clicked
-		//temp assume that it works and call is checkSquare
-		checkSquare(i);
-		return(){
-			//Show who's turn it is
-			<div className = "turn">{turn}</div>
-			<div className = "currentBoard">
+class Board extends Component {	
+	checkSquare(item) {
+		//
+	  }
+	  render() {
+	    return (
+	      <div>
+	        <div className="row">
+	          {this.checkSquare(0)}
+	          {this.checkSquare(1)}
+	          {this.checkSquare(2)}
+	        </div>
+	        <div className="row">
+	          {this.checkSquare(3)}
+	          {this.checkSquare(4)}
+	          {this.checkSquare(5)}
+	        </div>
+	        <div className="row">
+	          {this.checkSquare(6)}
+	          {this.checkSquare(7)}
+	          {this.checkSquare(8)}
+	        </div>
+	      </div>
+	    );
+	  }
+	}	
 
-				<div className = "row">
-					{this.checkSquare(0)}
-					{this.checkSquare(1)}
-					{this.checkSquare(2)}
-				</div>
-				<div className = "row">
-					{this.checkSquare(3)}
-					{this.checkSquare(4)}
-					{this.checkSquare(5)}
-				</div>
-				<div className = "row">
-					{this.checkSquare(6)}
-					{this.checkSquare(7)}
-					{this.checkSquare(8)}
-				</div>
-			}
-
-		}
-	}
-
+class Game extends Component {
+	
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+<script>
+	window.addEventListener('click', function(){
+		console.log('square has been clicked');
+	})
+
+</script>
