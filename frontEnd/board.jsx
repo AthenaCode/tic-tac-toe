652101d9cd3fcc3f8190dba3f6f5fb3c55eebd0e
Rachel <racheldepriest1@gmail.com>
2017-03-06 16:32:01 -0600

    started on front end

---
+++ frontEnd/board.jsx
@@ -0,0 +1,26 @@
+//visualization of Board
+
+//[0, 0, 0]
+//[0, 0, 0]
+//[0, 0, 0]
+
+import { Component } from 'react';
+
+class Squares extends Component{
+	render(){
+		return(){
+			<div className = "squares"></div>
+		}
+	}
+}
+
+class Board extends Component{
+	render(){
+		//create a function to check whether or not one of the squares has been clicked
+		//temp assume that it works and call is checkSquare
+		checkSquare(i);
+		return(){
+
+		}
+	}
+}
