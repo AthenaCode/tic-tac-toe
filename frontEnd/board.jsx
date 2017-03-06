279a220d782bb4155a7f7dfbb46cd1d854601588
Rachel <racheldepriest1@gmail.com>
2017-03-06 16:39:35 -0600

    created basic board

--- frontEnd/board.jsx
+++ frontEnd/board.jsx
@@ -4,6 +4,7 @@
 //[0, 0, 0]
 //[0, 0, 0]
 
+
 import { Component } from 'react';
 
 class Squares extends Component{
@@ -20,7 +21,27 @@ class Board extends Component{
 		//temp assume that it works and call is checkSquare
 		checkSquare(i);
 		return(){
+			//Show who's turn it is
+			<div className = "turn">{turn}</div>
+			<div className = "currentBoard">
+
+				<div className = "row">
+					{this.checkSquare(0)}
+					{this.checkSquare(1)}
+					{this.checkSquare(2)}
+				</div>
+				<div className = "row">
+					{this.checkSquare(3)}
+					{this.checkSquare(4)}
+					{this.checkSquare(5)}
+				</div>
+				<div className = "row">
+					{this.checkSquare(6)}
+					{this.checkSquare(7)}
+					{this.checkSquare(8)}
+				</div>
+			}
 
 		}
 	}
-}
+
